47b02543acc224e3e9521490a97d60f23e15a679
Fernando <afas@academico.ufpb.br>
2024-08-07 18:59:29 -0300

    * correct

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST='{"components/PivotTable.tsx -> react-pivottable/PivotTableUI":{"id":3037,"files":["static/chunks/37.778987fbe1e6c166.js"]}}';
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vUhowjz3d+xW0Ty/aOUbb1jGKk6MPQraNezs1UTUNz8=\"}"
